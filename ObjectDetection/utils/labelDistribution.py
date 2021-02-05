fb586a1cdb5dab5e46e106419d06b08cc3b7c382
Julien DELCLOS <julien.delclos@gmail.com>
2021-02-05 14:43:07 +0100

    Label distribution for data subfolder

--- ObjectDetection/utils/labelDistribution.py
+++ ObjectDetection/utils/labelDistribution.py
@@ -6,12 +6,12 @@ import matplotlib.pyplot as plt
 
 from tqdm import tqdm
 
-path_data = 'data'
+_path_data = 'data'
 
-typeSet = ['train', 'test', 'valid']
+_typeSet = ['train', 'test', 'valid']
 
-cls = ['black-bishop', 'black-king', 'black-knight', 'black-pawn', 'black-queen', 'black-rook',
-       'white-bishop', 'white-king', 'white-knight', 'white-pawn', 'white-queen', 'white-rook']
+_cls = ['black-bishop', 'black-king', 'black-knight', 'black-pawn', 'black-queen', 'black-rook',
+        'white-bishop', 'white-king', 'white-knight', 'white-pawn', 'white-queen', 'white-rook']
 
 # Matplotlib utils
 def autolabel(ax, rects, offset, percent=False):
@@ -22,12 +22,45 @@ def autolabel(ax, rects, offset, percent=False):
                 fontsize='x-small',
                 ha='center', va='bottom')
 
+def save_bars_plot(data, legends, ylabel, path, title, percent=False):
+    ind = np.arange(len(_cls))
+    width = 0.8 / len(legends)
+
+    fig, ax = plt.subplots()
+    ax.set_ylabel(ylabel)
+    ax.set_xticks(ind + (0.8 - width) / 2)
+    ax.set_xticklabels(_cls)
+    plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='right', fontsize='x-small')
+
+    if percent:
+        counts = [[x / data[label]['tot_boxes'] for x in data[label]['count']] for label in legends]
+    else:
+        counts = [data[label]['count'] for label in legends]
+
+    offset_text = 0.01 * max([max(counts[i]) for i in range(len(legends))])
+
+    rects = []
+    for i in range(len(legends)):
+        rect = ax.bar(
+                ind + width * i,
+                counts[i],
+                width=width)
+        autolabel(ax, rect, offset_text, percent=percent)
+        rects.append(rect)
+
+    ax.legend(rects, legends)
+    plt.title(path[path.find(_path_data)+len(_path_data)+1:])
+
+    fig.savefig(f'{path}/{title}.png')
+    plt.close(fig)
+    print(f'  {path}/{title}.png saved...')
+
 def create_data_counter(path):
     data_counter = {}
     data_counter['path_to_data'] = path
     data_counter['nb_files'] = 0
     data_counter['tot_boxes'] = 0
-    data_counter['count'] = [0 for i in range(len(cls))]
+    data_counter['count'] = [0 for i in range(len(_cls))]
     return data_counter
 
 def count_labels(path_labels):
@@ -46,12 +79,23 @@ def count_labels(path_labels):
 
     return data_counter
 
+def merge_data_folder(data_folder, path):
+    merged_counter = create_data_counter(path)
+
+    for subdir, counter in data_folder.items():
+        merged_counter['nb_files'] += counter['nb_files']
+        merged_counter['tot_boxes'] += counter['tot_boxes']
+        for i in range(len(_cls)):
+            merged_counter['count'][i] += counter['count'][i]
+
+    return merged_counter
+
 def analyse_folder(path):
     data_folder = {}
 
     plt_labels=[]
 
-    for subdirectory in typeSet:
+    for subdirectory in _typeSet:
         path_subdirectory = os.path.join(path_directory, subdirectory)
 
         # Ignore if the subdirectory doesn't exists
@@ -70,68 +114,30 @@ def analyse_folder(path):
         plt_labels.append(subdirectory)
 
     if len(plt_labels) > 0:
-        ind = np.arange(len(cls))
+        # Percentage figure
+        save_bars_plot(data=data_folder, legends=plt_labels,
+                       ylabel='% of appearance within each set',
+                       path=path, title='labels_distribution_percent',
+                       percent=True)
 
-        # PERCENTAGE FIGURE
-        fig, ax = plt.subplots()
-        ax.set_ylabel('% of appearances within each set')
-        ax.set_xticks(ind + 0.2)
-        ax.set_xticklabels(cls)
-        plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='right',fontsize='x-small')
-
-        counts_percent = [[x / data_folder[label]['tot_boxes'] for x in data_folder[label]['count']] for label in plt_labels]
-        offset_text = 0.01 * max([max(counts_percent[i]) for i in range(len(plt_labels))])
-
-        rects_percent = []
-        for i in range(len(plt_labels)):
-            rect = ax.bar(
-                    ind + 0.2 * i,
-                    counts_percent[i],
-                    width=0.2)
-            autolabel(ax, rect, offset_text, percent=True)
-            rects_percent.append(rect)
-
-        ax.legend(rects_percent, plt_labels)
-        plt.title(path[path.find(path_data)+len(path_data)+1:])
-
-        fig.savefig(f'{path}/labels_distribution_percent.png')
-        plt.close(fig)
-        print(f'  {path}/labels_distribution_percent.png saved...')
-
-        # NUMBER FIGURE
-        fig, ax1 = plt.subplots()
-        ax1.set_ylabel('# of appearances within each set')
-        ax1.set_xticks(ind + 0.2)
-        ax1.set_xticklabels(cls)
-        plt.setp(ax1.get_xticklabels(), rotation=30, horizontalalignment='right',fontsize='x-small')
-
-        offset_text = 0.01 * max([max(data_folder[label]['count']) for label in plt_labels])
-
-        rects = []
-        for i, subdirectory in enumerate(plt_labels):
-            rect = ax1.bar(
-                    ind + 0.2 * i,
-                    data_folder[subdirectory]['count'],
-                    width=0.2)
-            autolabel(ax1, rect, offset_text)
-            rects.append(rect)
-
-        ax1.legend(rects, plt_labels)
-        plt.title(path[path.find(path_data)+len(path_data)+1:])
-
-        fig.savefig(f'{path}/labels_distribution.png')
-        plt.close(fig)
-        print(f'  {path}/labels_distribution.png saved...')
+        # Number figure
+        save_bars_plot(data=data_folder, legends=plt_labels,
+                       ylabel='# of appearances within each set',
+                       path=path, title='labels_distribution',
+                       percent=False)
 
     return data_folder
 
 if __name__ == '__main__':
-    if not os.path.exists(path_data):
-        print(f'The path to the data "{path_data}" doesn\'t exists...')
+    if not os.path.exists(_path_data):
+        print(f'The path to the data "{_path_data}" doesn\'t exists...')
         exit()
 
-    for directory in os.listdir(path_data):
-        path_directory = os.path.join(path_data, directory)
+    data_all = {}
+    plt_directories = []
+
+    for directory in os.listdir(_path_data):
+        path_directory = os.path.join(_path_data, directory)
 
         # Ignore if this is not a folder
         if not os.path.isdir(path_directory):
@@ -139,6 +145,25 @@ if __name__ == '__main__':
 
         print(f'Directory {path_directory}...')
 
-        data_directory = analyse_folder(path_directory)
+        data_folder = analyse_folder(path_directory)
+        if len(data_folder) > 0:
+            data_all[directory] = merge_data_folder(data_folder, path_directory)
+            plt_directories.append(directory)
 
         print(f'Directory {path_directory}... Done\n')
+
+    print('Saving global data...')
+
+    # Percentage figure
+    save_bars_plot(data=data_all, legends=plt_directories,
+                   ylabel='% of appearance within each directories',
+                   path=_path_data, title='labels_distribution_percent',
+                   percent=True)
+
+    # Number figure
+    save_bars_plot(data=data_all, legends=plt_directories,
+                   ylabel='# of appearances within each directories',
+                   path=_path_data, title='labels_distribution',
+                   percent=False)
+
+    print('Done')
