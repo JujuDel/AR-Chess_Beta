d91f7b38a19ce31b36a46739ea12b6a9ad8665f9
Julien DELCLOS <julien.delclos@gmail.com>
2021-02-05 09:02:30 +0100

    Distribution both in percent and by number

--- ObjectDetection/utils/labelDistribution.py
+++ ObjectDetection/utils/labelDistribution.py
@@ -14,11 +14,11 @@ cls = ['black-bishop', 'black-king', 'black-knight', 'black-pawn', 'black-queen'
        'white-bishop', 'white-king', 'white-knight', 'white-pawn', 'white-queen', 'white-rook']
 
 # Matplotlib utils
-def autolabel(ax, rects, offset):
+def autolabel(ax, rects, offset, percent=False):
     for rect in rects:
         h = rect.get_height()
         ax.text(rect.get_x() + rect.get_width() / 2.,
-                h + offset, f'{round(100*h)}%',
+                h + offset, (f'{h}', f'{round(100*h)}%')[percent],
                 fontsize='x-small',
                 ha='center', va='bottom')
 
@@ -32,12 +32,12 @@ def create_data_counter(path):
 
 def count_labels(path_labels):
     data_counter = create_data_counter(path_labels)
-    
+
     for path_txt in tqdm(glob.glob(f'{path_labels}/*.txt')):
         data_counter['nb_files'] += 1
         with open(path_txt) as file:
             lines = file.read().split('\n')
-        
+
         for line in lines:
             if len(line) < 1: continue
             label = int(line.split()[0])
@@ -49,15 +49,9 @@ def count_labels(path_labels):
 def analyse_folder(path):
     data_folder = {}
 
-    fig, ax = plt.subplots()
-    ind = np.arange(len(cls))
-
-    rects=[]
     plt_labels=[]
 
-    offset_text = 0
-        
-    for i, subdirectory in enumerate(typeSet):
+    for subdirectory in typeSet:
         path_subdirectory = os.path.join(path_directory, subdirectory)
 
         # Ignore if the subdirectory doesn't exists
@@ -72,27 +66,59 @@ def analyse_folder(path):
         print(f'  {subdirectory}...')
 
         data_folder[subdirectory] = count_labels(path_subdirectory_labels)
-        
-        data_folder[subdirectory]['count'] = [x / data_folder[subdirectory]['tot_boxes'] for x in data_folder[subdirectory]['count']]
 
-        offset_text = max(offset_text, max(data_folder[subdirectory]['count']))
-        rects.append(ax.bar(ind + 0.2 * i, data_folder[subdirectory]['count'], width=0.2))
         plt_labels.append(subdirectory)
-    
-    if len(rects) > 0:
+
+    if len(plt_labels) > 0:
+        ind = np.arange(len(cls))
+
+        # PERCENTAGE FIGURE
+        fig, ax = plt.subplots()
         ax.set_ylabel('% of appearances within each set')
         ax.set_xticks(ind + 0.2)
         ax.set_xticklabels(cls)
         plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='right',fontsize='x-small')
 
-        offset_text = 0.01 * offset_text
-        
-        for rect in rects:
-            autolabel(ax, rect, offset_text)
-    
-        ax.legend(rects, plt_labels)
+        counts_percent = [[x / data_folder[label]['tot_boxes'] for x in data_folder[label]['count']] for label in plt_labels]
+        offset_text = 0.01 * max([max(counts_percent[i]) for i in range(len(plt_labels))])
+
+        rects_percent = []
+        for i in range(len(plt_labels)):
+            rect = ax.bar(
+                    ind + 0.2 * i,
+                    counts_percent[i],
+                    width=0.2)
+            autolabel(ax, rect, offset_text, percent=True)
+            rects_percent.append(rect)
+
+        ax.legend(rects_percent, plt_labels)
         plt.title(path[path.find(path_data)+len(path_data)+1:])
-        
+
+        fig.savefig(f'{path}/labels_distribution_percent.png')
+        plt.close(fig)
+        print(f'  {path}/labels_distribution_percent.png saved...')
+
+        # NUMBER FIGURE
+        fig, ax1 = plt.subplots()
+        ax1.set_ylabel('# of appearances within each set')
+        ax1.set_xticks(ind + 0.2)
+        ax1.set_xticklabels(cls)
+        plt.setp(ax1.get_xticklabels(), rotation=30, horizontalalignment='right',fontsize='x-small')
+
+        offset_text = 0.01 * max([max(data_folder[label]['count']) for label in plt_labels])
+
+        rects = []
+        for i, subdirectory in enumerate(plt_labels):
+            rect = ax1.bar(
+                    ind + 0.2 * i,
+                    data_folder[subdirectory]['count'],
+                    width=0.2)
+            autolabel(ax1, rect, offset_text)
+            rects.append(rect)
+
+        ax1.legend(rects, plt_labels)
+        plt.title(path[path.find(path_data)+len(path_data)+1:])
+
         fig.savefig(f'{path}/labels_distribution.png')
         plt.close(fig)
         print(f'  {path}/labels_distribution.png saved...')
